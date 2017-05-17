58b1c0e65f0b93a1c990281c1fae5592d6f14f21
Kamil Poznakowski <kamil.poznakowski@kaseka.lan>
2017-05-17 19:22:38 +0200

    kolorowe kwadraciki cd

--- app/src/main/java/com/kampoz/sketchat/dialog/ColorPickerDialogFragment.java
+++ app/src/main/java/com/kampoz/sketchat/dialog/ColorPickerDialogFragment.java
@@ -5,22 +5,22 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import com.kampoz.sketchat.R;
 import com.kampoz.sketchat.shape.ColorCircleView;
 
-/**
- * Created by wasili on 2017-05-17.
- */
-
 public class ColorPickerDialogFragment extends DialogFragment {
   Paint paint = new Paint();
-  ImageView drawingImageView;
+  //ImageView drawingImageView;
 
   @Override
   public Dialog onCreateDialog(Bundle ssvadInstanceState) {
@@ -31,19 +31,70 @@ public class ColorPickerDialogFragment extends DialogFragment {
 
     ColorCircleView  colorCircleView = new ColorCircleView(getActivity());
 
-    drawingImageView = (ImageView) view.findViewById(R.id.vColorCircle);
-    Bitmap bitmap = Bitmap.createBitmap(500
-        ,500, Bitmap.Config.ARGB_8888);
-    Canvas canvas = new Canvas(bitmap);
-    drawingImageView.setImageBitmap(bitmap);
+    final ImageView drawingImageView = (ImageView) view.findViewById(R.id.ivColorsContainet);
+    ViewTreeObserver vto = drawingImageView.getViewTreeObserver();
+    vto.addOnGlobalLayoutListener (new OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        drawingImageView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        int parentWidth = ((LinearLayout)drawingImageView.getParent()).getWidth();
 
-    Paint paint = new Paint();
-    paint.setColor(Color.RED);
-    paint.setStyle(Paint.Style.STROKE);
-    float x = 250;
-    float y = 250;
-    float radius = 200;
-    canvas.drawCircle(x, y, radius, paint);
+        int drawingImageViewWidth  = parentWidth-50;
+        int drawingImageViewHeight  = parentWidth-50;
+        int singleSquareWidth = drawingImageViewWidth/10;
+
+        //int drawingImageViewWidth  = drawingImageView.getLayoutParams().height;
+        //int drawingImageViewHeight = drawingImageView.getLayoutParams().width;
+
+        Bitmap bitmap = Bitmap.createBitmap(drawingImageViewWidth, drawingImageViewHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawingImageView.setImageBitmap(bitmap);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        float x = 250;
+        float y = 250;
+        float radius = 100;
+        canvas.drawCircle(x, y, radius, paint);
+
+        int red, green, blue;
+        float left, top, right, bottom;
+
+        left = 0;
+        top = 0;
+        right = 0;
+        bottom = 0;
+
+        red = 255;
+        green = 0;
+        blue = 0;
+
+        int counter = 0;
+
+        for(int i = 0; i< drawingImageViewWidth; i+=singleSquareWidth){
+          for(int j = 0; j< drawingImageViewWidth; j+=singleSquareWidth) {
+            paint.setStyle(Style.FILL);
+            paint.setColor(Color.rgb(red, green, blue));
+            canvas.drawRect(j, i, j+singleSquareWidth, i+singleSquareWidth, paint);
+            //red =+ 3;
+            green+=25;
+            if(counter>33){
+              blue+=25;
+            }
+            if(counter>66) {
+              red -= 25;
+            }
+            counter +=1;
+          }
+        }
+
+//        paint.setStyle(Style.FILL);
+//        canvas.drawRect(200,200,250,250, paint);
+//        paint.setColor(Color.rgb(0,0,100));
+//        canvas.drawRect(250,200,300,250, paint);
+      }
+    });
 
     builder.setView(view);
     Dialog dialog = builder.create();
